773575dc788e1d628684f7321d9a5764a2a5e8ae
John Hoffer <john@hoff.in>
2026-02-19 21:25:49 +0000

    update

---
+++ assets/raw-hHbwtWAx.js
@@ -0,0 +1,9 @@
+import { B as BaseDecoder } from "./decoder.worker-BnFrV31n.js";
+class RawDecoder extends BaseDecoder {
+  decodeBlock(buffer) {
+    return buffer;
+  }
+}
+export {
+  RawDecoder as default
+};
