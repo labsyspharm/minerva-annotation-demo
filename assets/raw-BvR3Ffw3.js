eee925a827bf2bc88f2e7bd7a8a996df76b7112d
John Hoffer <john@hoff.in>
2026-03-05 19:39:19 +0000

    update

---
+++ assets/raw-BvR3Ffw3.js
@@ -0,0 +1,9 @@
+import { B as BaseDecoder } from "./index-DWFbF8-A.js";
+class RawDecoder extends BaseDecoder {
+  decodeBlock(buffer) {
+    return buffer;
+  }
+}
+export {
+  RawDecoder as default
+};
