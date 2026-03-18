974287615bf2c1a414bd314ff5ada4bffe14ccec
John Hoffer <john@hoff.in>
2026-03-18 18:03:44 +0000

    update

--- assets/raw-BASrHlkY.js
+++
@@ -1,9 +0,0 @@
-import { B as BaseDecoder } from "./index-D8z8S5i2.js";
-class RawDecoder extends BaseDecoder {
-  decodeBlock(buffer) {
-    return buffer;
-  }
-}
-export {
-  RawDecoder as default
-};
