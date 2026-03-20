9ad83dc1b325b43623185c611f7ef86ac94e6f98
John Hoffer <john@hoff.in>
2026-03-20 19:39:47 +0000

    update

---
+++ assets/raw-CTUNO_ix.js
@@ -0,0 +1,9 @@
+import { B as BaseDecoder } from "./index-CdfD0tXh.js";
+class RawDecoder extends BaseDecoder {
+  decodeBlock(buffer) {
+    return buffer;
+  }
+}
+export {
+  RawDecoder as default
+};
