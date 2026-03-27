9d5c02e1e2714464f433f62f54dbf96d40ef744b
John Hoffer <john@hoff.in>
2026-03-27 19:17:22 +0000

    update

--- assets/raw-D_fEMWdb.js
+++
@@ -1,9 +0,0 @@
-import { B as BaseDecoder } from "./index-Cz44F1qD.js";
-class RawDecoder extends BaseDecoder {
-  decodeBlock(buffer) {
-    return buffer;
-  }
-}
-export {
-  RawDecoder as default
-};
