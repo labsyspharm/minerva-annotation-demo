4d405fcb53281e722e1ab14c1c7a4fcb56bb59ae
John Hoffer <john@hoff.in>
2026-04-03 19:11:34 +0000

    update

---
+++ assets/raw-CVGk_yMm.js
@@ -0,0 +1,9 @@
+import { B as BaseDecoder } from "./index-rRw7TS46.js";
+class RawDecoder extends BaseDecoder {
+  decodeBlock(buffer) {
+    return buffer;
+  }
+}
+export {
+  RawDecoder as default
+};
