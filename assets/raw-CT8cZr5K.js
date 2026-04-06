425b68b2790cd13042add11c7aec6369ccdeed6d
John Hoffer <john@hoff.in>
2026-04-06 18:24:52 +0000

    update

--- assets/raw-CT8cZr5K.js
+++
@@ -1,9 +0,0 @@
-import { B as BaseDecoder } from "./index-Ch10mSCS.js";
-class RawDecoder extends BaseDecoder {
-  decodeBlock(buffer) {
-    return buffer;
-  }
-}
-export {
-  RawDecoder as default
-};
