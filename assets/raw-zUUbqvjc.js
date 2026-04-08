35097be26b25fd30fdb3c76c5f0d9782c533344e
John Hoffer <john@hoff.in>
2026-04-08 17:27:46 +0000

    update

---
+++ assets/raw-zUUbqvjc.js
@@ -0,0 +1,9 @@
+import { B as BaseDecoder } from "./index-B6ExrLiv.js";
+class RawDecoder extends BaseDecoder {
+  decodeBlock(buffer) {
+    return buffer;
+  }
+}
+export {
+  RawDecoder as default
+};
