bcb0c551e04c9cfd55e49f278b5930cef80acd22
John Hoffer <john@hoff.in>
2026-04-10 18:22:52 +0000

    update

--- assets/raw-CwSrBRnX.js
+++
@@ -1,9 +0,0 @@
-import { B as BaseDecoder } from "./index-DezXcHg1.js";
-class RawDecoder extends BaseDecoder {
-  decodeBlock(buffer) {
-    return buffer;
-  }
-}
-export {
-  RawDecoder as default
-};
