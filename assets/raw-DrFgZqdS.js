01614e805f53549a54a9ea6d1ac3e8a536af7a2a
John Hoffer <john@hoff.in>
2026-04-10 17:24:10 +0000

    update

--- assets/raw-DrFgZqdS.js
+++
@@ -1,9 +0,0 @@
-import { B as BaseDecoder } from "./index-C0582GsN.js";
-class RawDecoder extends BaseDecoder {
-  decodeBlock(buffer) {
-    return buffer;
-  }
-}
-export {
-  RawDecoder as default
-};
